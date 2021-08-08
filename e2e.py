5cae151f3e504419f3cb9e9efc7130555ffe3b53
imanuelg-GitHub <85810404+imanuelg-GitHub@users.noreply.github.com>
2021-08-08 23:09:14 +0100

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 
 
 def test_scores_service(url_score_server):
-    chrome_driver = webdriver.Chrome(executable_path="c:/temp/chromedriver.exe")
-    chrome_driver.get(url_score_server)
+    Edge_driver = webdriver.Edge(executable_path="C:/Users/imanu/Downloads/edgedriver_win32/msedgedriver.exe")
+    Edge_driver.get(url_score_server)
     # score_result = chrome_driver.find_element_by_id("score").text
-    chrome_driver.close()
+    Edge_driver.close()
     #return int(score_result) in range(1, 1000)
     return int(234)
 
