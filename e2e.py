34d0244180cd1c0f7d53760008b8921cf57094bc
imanuelg-GitHub <85810404+imanuelg-GitHub@users.noreply.github.com>
2021-08-08 16:52:59 +0100

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -2,9 +2,9 @@ import os
 from selenium import webdriver
 
 
-def test_scores_service():
+def test_scores_service(url):
     Edge_driver = webdriver.Edge(executable_path="C:/Users/imanu/Downloads/edgedriver_win32/msedgedriver.exe")
-    Edge_driver.get("http://localhost:8777/")
+    Edge_driver.get(url)
     score = Edge_driver.find_element_by_tag_name("scores").text
     if 1 <= int(score) <= 1000:
         return True
@@ -12,7 +12,7 @@ def test_scores_service():
 
 
 def main():
-    if test_scores_service():
+    if test_scores_service("http://localhost:8777/"):
         return 0
     else:
         os._exit(-1)
